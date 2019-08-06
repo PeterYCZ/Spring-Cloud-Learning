25d75802d20f953e88f371c5b2cbbfed3a247e94
PeterYCZ <yecongzhoupeter@qq.com>
2019-08-07 01:01:38 +0800

    add producer

---
+++ eurekaproducer/src/main/java/com/learing/springcloud/eurekaproducer/EurekaproducerApplication.java
@@ -0,0 +1,13 @@
+package com.learing.springcloud.eurekaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaproducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaproducerApplication.class, args);
+    }
+
+}
---
+++ eurekaproducer/src/main/java/com/learing/springcloud/eurekaproducer/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.learing.springcloud.eurekaproducer.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @Value("${config.producer.instance:0}")
+    private int instance;
+
+    @GetMapping("/")
+    public String hello(@RequestParam String name) {
+        return "[" + instance + "]" + "Hello, " + name + " " + new Date();
+    }
+
+}
