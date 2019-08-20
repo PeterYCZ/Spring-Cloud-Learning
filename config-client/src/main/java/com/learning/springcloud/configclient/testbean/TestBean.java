814cea14274964b1d2a5313de08cafdc5ebe8a68
ycz <yecongzhoupeter@qq.com>
2019-08-20 22:21:46 +0800

    add testBean

--- config-client/src/main/java/com/learning/springcloud/configclient/testbean/TestBean.java
+++ config-client/src/main/java/com/learning/springcloud/configclient/testbean/TestBean.java
@@ -1,4 +1,21 @@
 package com.learning.springcloud.configclient.testbean;
 
-public class TestBean {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@RefreshScope
+@Component
+public class TestBean implements HealthIndicator {
+
+    @Value("${neo.hello:error}")
+    private String profile;
+
+
+    @Override
+    public Health health() {
+        return Health.up().withDetail("context", profile).build();
+    }
 }
