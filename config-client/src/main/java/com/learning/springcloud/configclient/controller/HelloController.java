fc6a6ba74bc72492f7848791b7b3be395a91868d
ycz <yecongzhoupeter@qq.com>
2019-08-20 22:03:06 +0800

    test refrash config-client

--- config-client/src/main/java/com/learning/springcloud/configclient/controller/HelloController.java
+++ config-client/src/main/java/com/learning/springcloud/configclient/controller/HelloController.java
@@ -1,4 +1,20 @@
 package com.learning.springcloud.configclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RefreshScope
 public class HelloController {
+
+    @Value("${neo.hello:error}")
+    private String profile;
+
+    @GetMapping("/info")
+    public Mono<String> hello() {
+        return Mono.justOrEmpty(profile);
+    }
 }
